7cda802557b723d5a0176bebc25b896424492092
W.S.Amanda <e19017@eng.pdn.ac.lk>
2023-12-06 19:03:10 +0530

    startup page changed

--- shopwise/lib/login_screen.dart
+++ shopwise/lib/login_screen.dart
@@ -7,7 +7,7 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           "ShopWise",
         ),
       ),
--- shopwise/lib/main.dart
+++ shopwise/lib/main.dart
@@ -19,7 +19,6 @@ class MyApp extends StatelessWidget {
               // useMaterial3: true,
               )
           .copyWith(
-        useMaterial3: true,
         primaryColor: Colors.white,
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
@@ -31,7 +30,7 @@ class MyApp extends StatelessWidget {
         ),
       ),
       // home: const MyHomePage(title: 'Shop Wise'),
-      home: const LoginScreen(),
+      home: const StartupPage(),
     );
   }
 }
@@ -56,21 +55,11 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: const Text('Shop Wise'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
