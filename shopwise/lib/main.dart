e005728a6fb31383a3e4d346f4c9d12dcbbf5055
jali-k <e19210@pdn.ac.lk>
2023-12-11 21:16:10 +0530

    enable multidex suppory

--- shopwise/lib/main.dart
+++ shopwise/lib/main.dart
@@ -68,21 +68,9 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text('Shop Wise'),
       ),
-      body: Center(
+      body: const Center(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
+  
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
