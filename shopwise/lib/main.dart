6a3a5be723c89503e8108d042dbf2c2b119fa9eb
jali-k <e19210@pdn.ac.lk>
2024-01-05 16:31:46 +0530

    Initialize amplify auth

--- shopwise/lib/main.dart
+++ shopwise/lib/main.dart
@@ -1,10 +1,13 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
 import 'package:flutter/material.dart';
 import 'package:shopwise/pages/login_screen.dart';
 import 'package:shopwise/pages/products_list_page.dart';
 import 'package:shopwise/pages/register.dart';
 import 'package:shopwise/pages/startup.dart';
 import 'package:shopwise/utils/colors.dart';
+
+import 'amplifyconfiguration.dart';
 
 // void main() async{
 //   WidgetsFlutterBinding.ensureInitialized();
@@ -16,10 +19,35 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+
+@override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _configureAmplify();
+  }
+
+  Future<void> _configureAmplify() async {
+    try {
+      final auth = AmplifyAuthCognito();
+      await Amplify.addPlugin(auth);
+
+      // call Amplify.configure to use the initialized categories in your app
+      await Amplify.configure(amplifyconfig);
+    } on Exception catch (e) {
+      safePrint('An error occurred configuring Amplify: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
