e7dd2ca5e39c9bd5a02dd8a32664db3b4461fd8d
jali-k <e19210@eng.pdn.ac.lk>
2024-01-22 16:44:18 +0530

    Add Customer model class

---
+++ shopwise/lib/models/customer.dart
@@ -0,0 +1,9 @@
+class Customer {
+  final String sub_uuid;
+  final String hashcode;
+  final String order_id;
+  final DateTime shopping_date;
+
+Customer({required this.sub_uuid, required this.hashcode, required this.order_id, required this.shopping_date });
+
+}
