71b38bb46bf17bb57261860f79947ed1f6ee5a7f
jali-k <e19210@eng.pdn.ac.lk>
2024-01-22 22:36:56 +0530

    Create method to save the cutomer in firebase

--- shopwise/lib/providers/customer_provider.dart
+++ shopwise/lib/providers/customer_provider.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import "package:riverpod/riverpod.dart";
 import 'package:shopwise/models/customer.dart';
 
@@ -11,7 +12,22 @@ class CustomerNotifier extends StateNotifier<Customer> {
 
   void updateCustomer(Customer customer) {
     state = customer;
+    saveCustomer(state);
   }
+}
+
+// Method to save the state in firebase database, 'customers' collection
+void saveCustomer(Customer customer) {
+  // Get a reference to the database collection 'customers'
+  final customerCollection = FirebaseFirestore.instance.collection('customers');
+
+  // Call the method to save the customer
+  customerCollection.add({
+    'sub_uuid': customer.sub_uuid,
+    'hashcode': customer.hashcode,
+    'order_id': customer.order_id,
+    'shopping_date': customer.shopping_date,
+  });
 }
 
 final customerNotifierProvider =
