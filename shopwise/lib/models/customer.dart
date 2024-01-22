371fe514439b92854d38818ec69746bb2c5355cc
jali-k <e19210@eng.pdn.ac.lk>
2024-01-22 23:09:03 +0530

    Separaet methods to update customer

--- shopwise/lib/models/customer.dart
+++ shopwise/lib/models/customer.dart
@@ -6,4 +6,18 @@ class Customer {
 
 Customer({required this.sub_uuid, required this.hashcode, required this.order_id, required this.shopping_date });
 
+Customer copyWith({
+    String? order_id,
+    String? sub_uuid,
+    String? hashcode,
+    DateTime? shopping_date,
+  }) {
+    return Customer(
+      order_id: order_id ?? this.order_id,
+      sub_uuid: sub_uuid ?? this.sub_uuid,
+      hashcode: hashcode ?? this.hashcode,
+      shopping_date: shopping_date ?? this.shopping_date,
+    );
+  }
+
 }
--- shopwise/lib/providers/customer_provider.dart
+++ shopwise/lib/providers/customer_provider.dart
@@ -12,23 +12,56 @@ class CustomerNotifier extends StateNotifier<Customer> {
 
   void updateCustomer(Customer customer) {
     state = customer;
-    saveCustomer(state);
+
+  }
+
+  // Create seperate methods to update each field
+  void updateSubUuid(String sub_uuid) {
+    state = state.copyWith(sub_uuid: sub_uuid);
+  }
+
+  void updateHashcode(String hashcode) {
+    state = state.copyWith(hashcode: hashcode);
+  }
+  
+  void updateOrderId(String order_id) {
+    state = state.copyWith(order_id: order_id);
+  }
+
+  void updateShoppingDate(DateTime shopping_date) {
+    state = state.copyWith(shopping_date: shopping_date);
+  }
+
+  // Method to save the state in firebase database, 'customers' collection
+  void saveCustomer() {
+    // Get a reference to the database collection 'customers'
+    final customerCollection =
+        FirebaseFirestore.instance.collection('customers');
+
+    // Call the method to save the customer
+    customerCollection.add({
+      'sub_uuid': state.sub_uuid,
+      'hashcode': state.hashcode,
+      'order_id': state.order_id,
+      'shopping_date': state.shopping_date,
+    });
   }
 }
 
-// Method to save the state in firebase database, 'customers' collection
-void saveCustomer(Customer customer) {
-  // Get a reference to the database collection 'customers'
-  final customerCollection = FirebaseFirestore.instance.collection('customers');
+// // Method to save the state in firebase database, 'customers' collection
+// void saveCustomer(Customer customer) {
+//   // Get a reference to the database collection 'customers'
+//   final customerCollection = FirebaseFirestore.instance.collection('customers');
 
-  // Call the method to save the customer
-  customerCollection.add({
-    'sub_uuid': customer.sub_uuid,
-    'hashcode': customer.hashcode,
-    'order_id': customer.order_id,
-    'shopping_date': customer.shopping_date,
-  });
-}
+//   // Call the method to save the customer
+//   customerCollection.add({
+//     'sub_uuid': customer.sub_uuid,
+//     'hashcode': customer.hashcode,
+//     'order_id': customer.order_id,
+//     'shopping_date': customer.shopping_date,
+//   });
+// }
+
 
 final customerNotifierProvider =
     StateNotifierProvider<CustomerNotifier, Customer>(
