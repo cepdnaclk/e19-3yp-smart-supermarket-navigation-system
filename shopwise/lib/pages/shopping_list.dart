a06f422a1cc275fb7b0b6637b4b279756d74bf7b
jali-k <e19210@eng.pdn.ac.lk>
2024-01-23 16:39:30 +0530

    Save the added items of the shopping list to firebase

--- shopwise/lib/pages/shopping_list.dart
+++ shopwise/lib/pages/shopping_list.dart
@@ -1,20 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod/riverpod.dart';
 import 'package:shopwise/models/product.dart';
+import 'package:shopwise/pages/scan_barcode_view.dart';
+import 'package:shopwise/pages/scan_qrcode_page.dart';
 import 'package:shopwise/pages/select_items.dart';
+import 'package:shopwise/providers/customer_provider.dart';
 import 'package:shopwise/services/mongodb_service.dart';
 import 'package:shopwise/widgets/added_item.dart';
 
-class ShoppingList extends StatefulWidget {
+class ShoppingList extends ConsumerStatefulWidget {
   static const String routeName = '/shoppingList';
   ShoppingList({super.key});
   final List<Product> shoppingList = <Product>[];
 
   @override
-  State<ShoppingList> createState() => _ShoppingListState();
+  ConsumerState<ShoppingList> createState() => _ShoppingListState();
 }
 
-class _ShoppingListState extends State<ShoppingList> {
-
+class _ShoppingListState extends ConsumerState<ShoppingList> {
   @override
   void dispose() {
     // TODO: implement dispose
@@ -22,12 +27,14 @@ class _ShoppingListState extends State<ShoppingList> {
 
     // Implement list save to MONGO DB
   }
+
   void addItem(Product product) {
     widget.shoppingList.add(product);
   }
 
   @override
   Widget build(BuildContext context) {
+    final ordersCollection = FirebaseFirestore.instance.collection('orders');
     return Scaffold(
       appBar: AppBar(
         title: const Text('Shopping List'),
@@ -78,9 +85,9 @@ class _ShoppingListState extends State<ShoppingList> {
                     print(widget.shoppingList.length);
                     setState(() {});
                   }
-                  MongoDB_Service.initiateConnection();
-                  MongoDB_Service.insertData("products", {"title": "test"});
-                  MongoDB_Service.closeConnection();
+                  // MongoDB_Service.initiateConnection();
+                  // MongoDB_Service.insertData("products", {"title": "test"});
+                  // MongoDB_Service.closeConnection();
 
                   // if (result != null && result is Product) {
                   //   String newItem = result;
@@ -94,6 +101,26 @@ class _ShoppingListState extends State<ShoppingList> {
                 print(item);
               },
               child: Text("Add Item"),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green, // background
+                  foregroundColor: Colors.white // foreground
+                  ),
+              onPressed: () => {
+                ref.read(customerNotifierProvider.notifier).updateOrderId("5"),
+                ordersCollection.add({
+                  'id': '5',
+                  'products':
+                      widget.shoppingList.map((element) => element.id).toList()
+                }),
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => ScanBarcode(),
+                  ),
+                )
+              },
+              child: Text("Start Navigation"),
             )
           ],
         ),
