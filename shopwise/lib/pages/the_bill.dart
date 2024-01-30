272a04016bc9bf933071db59cb045740b2ee45c6
jali-k <e19210@eng.pdn.ac.lk>
2024-01-31 04:17:11 +0530

    Add the bill

--- shopwise/lib/pages/the_bill.dart
+++ shopwise/lib/pages/the_bill.dart
@@ -8,10 +8,56 @@ class TheBill extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text("The Bill"),
+          title: const Text("Check Your Total"),
         ),
-        body: Center(
-          child: Text("The bill"),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(children: [
+              Image.asset("assets/images/bill.jpg"),
+              ListTile(
+                trailing: Text("Rs. 500"),
+                title: Text("Vaseline × 1"),
+              ),
+              ListTile(
+                trailing: Text("Rs. 200"),
+                title: Text("Tooth Paste × 1"),
+              ),
+              ListTile(
+                trailing: Text("Rs. 120"),
+                title: Text("Tooth Brush × 1"),
+              ),
+              ListTile(
+                trailing: Text("Rs. 350"),
+                title: Text("Chocolate Biscuit × 1"),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.9,
+                height: 1,
+                color: Colors.green.shade500,
+              ),
+              ListTile(
+                trailing: Text(
+                  "Rs. 1170",
+                  style: TextStyle(
+                      color: Colors.green.shade500,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                ),
+                title: Text(
+                  "Total",
+                  style: TextStyle(
+                      color: Colors.green.shade500,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.9,
+                height: 2,
+                color: Colors.green.shade500,
+              ),
+            ]),
+          ),
         ));
   }
 }
